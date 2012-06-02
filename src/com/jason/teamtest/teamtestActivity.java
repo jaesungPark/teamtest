ecaeeaddf0f88aeef9cbd6c7b667bd48590abfad
jaesungpark <hotpjs1@gmail.com>
2012-06-02 16:23:02 +0900

    init

---
+++ src/com/jason/teamtest/teamtestActivity.java
@@ -0,0 +1,13 @@
+package com.jason.teamtest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class teamtestActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
